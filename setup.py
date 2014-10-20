8208a0649973de3a4774961f57503bf9519d6503
Ben Ockmore <ben.sput@gmail.com>
2014-10-20 23:22:05 +0100

    Fixed Pylint warning for setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='bbschema',
-      version='1.0',
-      description='Schema for the BookBrainz database',
-      author='Ben Ockmore',
-      author_email='ben.sput@gmail.com',
-      url='https://bitbucket.org/bookbrainz/schema',
-      packages=['bbschema'],
-      requires=['sqlalchemy (>=0.9.7)', 'psycopg2 (>=2.5.4)'],
-      provides=['bbschema'],
+setup(
+    name='bbschema',
+    version='1.0',
+    description='Schema for the BookBrainz database',
+    author='Ben Ockmore',
+    author_email='ben.sput@gmail.com',
+    url='https://bitbucket.org/bookbrainz/schema',
+    packages=['bbschema'],
+    requires=['sqlalchemy (>=0.9.7)', 'psycopg2 (>=2.5.4)'],
+    provides=['bbschema'],
 )
