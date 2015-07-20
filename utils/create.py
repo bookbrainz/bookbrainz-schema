d7aab70455cd58d908bb81629d351bef45a7be4e
Ben Ockmore <ben.sput@gmail.com>
2015-07-20 22:28:31 +0100

    Updated create.py to only create the tables we want, not all of mbdata.

--- utils/create.py
+++ utils/create.py
@@ -4,10 +4,17 @@ from sqlalchemy import create_engine
 
 from bbschema.base import Base
 
+INCLUDED_TABLES = [
+    'musicbrainz.language',
+    'musicbrainz.gender'
+]
+
 
 def create_all(hostname, port, username, password, db_name):
     connection_string =\
         'postgresql://{}:{}@{}/{}'.format(username, password, hostname,
                                           db_name)
     engine = create_engine(connection_string, echo=True)
-    Base.metadata.create_all(engine)
+    tables = [t for name, t in Base.metadata.tables.items()
+              if name.startswith('bookbrainz') or name in INCLUDED_TABLES]
+    Base.metadata.create_all(engine, tables=tables)
